36de0ad766d479448eab6fdf27952cf4ece99933
felipe <f_cordovafer@fpzornotza.com>
2025-05-23 12:23:52 +0200

    DiagramasyCasos Realizados

--- RETO_FINALIZADO_FINALIZADO/RETO/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"BBDD"},{"l":"Controlador"},{"l":"logs"},{"l":"Modelos"},{"l":"test"},{"l":"Ventana"}];updateSearchResults();
